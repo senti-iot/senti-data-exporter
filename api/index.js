200d008a0f3fcda8149db2841c9bbf74a6a13270
Andrei Tudor <andrei.tudor27@gmail.com>
2019-12-13 16:17:05 +0100

    Checkpoint

---
+++ api/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const router = express.Router()
+const verifyAPIVersion = require('senti-apicore').verifyapiversion
+const { authenticate } = require('senti-apicore')
+const getDeviceData = require('../data/devices/getDeviceData').getDeviceData
+
+router.post('/:version/export', async (req, res) => {
+	let version = req.params.version
+	let authToken = req.headers.auth
+	if (verifyAPIVersion(version)) {
+		if (authenticate(authToken)) {
+			let body = req.body
+			let data = await getDeviceData(body.config)
+			console.log('Processed data', data[0])
+			return res.send(data)
+		}
+		return res.status(500).json("Error: Invalid token")
+	}
+	return res.status(500).json("Error: Invalid Version")
+})
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 #!/usr/bin/env nodejs
+//eslint-disable-next-line
 const dotenv = require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const helmet = require('helmet')
 const app = express()
-
+const Endpoint = require('./api/index.js')
 // API endpoint imports
-
 const port = process.env.NODE_PORT || 3021
 
 app.use(helmet())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+/**
+ * API
+ */
+app.use([Endpoint])
 app.use(cors())
 
 
