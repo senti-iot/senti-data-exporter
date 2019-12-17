1c44d94eb81d27d75455ce57ba139e908b4aa17d
Andrei Tudor <andrei.tudor27@gmail.com>
2019-12-17 18:32:45 +0100

    console.log filters

--- data/filtering/index.js
+++ data/filtering/index.js
@@ -2,15 +2,15 @@ const moment = require('moment')
 
 const handleFilters = (filters, data) => {
 	let nData = data
-	if (nData && nData.length > 0) {
+	if (nData) {
 
 		filters.forEach(f => {
-
+			console.log('Filtering', f)
 			switch (f.type) {
 				case 'datetime':
 					console.log('filtering by datetime')
 					nData = nData.filter(d => {
-						return moment(d[f.key]).valueOf() >= moment(f.from).valueOf() && moment(d[f.key]).valueOf() <= moment(f.to).valueOf() ? true : false
+						return moment(d[f.key]).valueOf() >= moment(f.from).valueOf() && moment(d[f.key]).valueOf() <= moment(f.to).valueOf()
 					})
 					break;
 
