c37d94f52c05a7ec4fdc98a85bd893497c73d14c
Andrei Tudor <andrei.tudor27@gmail.com>
2021-02-17 15:31:41 +0100

    Total Usage for admins

--- api/auth.js
+++ api/auth.js
@@ -6,10 +6,11 @@ router.all('*', async (req, res, next) => {
 
 	let lease = await authClient.getLease(req)
 	let bearerToken = await authClient.parseBearerToken(req)
-	// if (lease === false) {
-	// 	res.status(401).json()
-	// 	return
-	// }
+	if (lease === false) {
+		res.status(401).json()
+		return
+	}
+	console.log('Authenticated')
 	req.bearer = bearerToken
 	req.lease = lease
 	next()
--- api/waterworks/usage.js
+++ api/waterworks/usage.js
@@ -3,27 +3,36 @@ const express = require('express')
 const databrokerAPI = require('../engine')
 const router = express.Router()
 
-router.get('/v2/waterworks/usage/:type/:from/:to', (req, res) => {
+router.get('/v2/waterworks/usage/:type/:orgUUID/:from/:to', async (req, res) => {
 
 	let type = req.params.type
 	let bearerToken = req.bearer
+	let from = req.params.from
+	let to = req.params.to
+	let orgUUID = req.params.orgUUID
 	/**
 	 *  Get the data
 	 * */
 	databrokerAPI.setHeader('Authorization', `Bearer ${bearerToken}`)
 
-	let data = null
+	let data = await databrokerAPI.get(`/v2/waterworks/data/totalusagebyday/${orgUUID}/${from}/${to}`).then(rs => rs.data)
 
 
 	/**
 	 * Send the data back
 	 */
-	res.setHeader('Content-Type', 'text/csv')
-	res.setHeader('Content-Disposition', 'attachment; filename=\"' + 'download-' + Date.now() + '.csv\"');
+
+	console.log('type', type)
 	switch (type) {
 		case 'csv':
-			return stringify(data, { header: true, delimiter: ';' })
-				.pipe(res)
+			res.setHeader('Content-Type', 'text/csv')
+			res.setHeader('Content-Disposition', 'attachment; filename=\"' + 'download-' + Date.now() + '.csv\"');
+
+			// await res.status(200).attachment('download-' + Date.now() + '.csv\"').send()
+			stringify(data, { header: true, delimiter: ';' }).pipe(res)
+			// stringify(data, { header: true, delimiter: ';' })
+			// .pipe(res)
+			return
 		case 'json':
 			return res.status(200).json(data)
 		default:
